9b2502369121f9e8aa8d3abd75ab6a3cb451e804
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-06-22 20:34:07 +0300

    Добавьте файлы проекта.

---
+++ Task19_4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include<fstream>
+int main()
+{
+    char sim[5];
+    std::ifstream img;
+    img.open("imege.png",std::ios::binary);
+    img.read(sim, sizeof(sim));
+    sim[4] = 0;
+    std::cout << sim << std::endl;
+
+    img.close();
+}
+
